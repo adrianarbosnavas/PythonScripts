504bc5011d7f1854edc166a97a5c985e86dfdc23
Adrián Arbós <adrianarbosnavas@users.noreply.github.com>
2017-12-01 10:21:17 +0100

    Create removeByExtension.py
    Elimina archivos de un directorio según extensiones especificadas.

---
+++ removeByExtension.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import os
+
+# Directory to loop
+rootdir = 'directory'
+
+# List of extensions to delete
+extToDelete = ['.ext']
+
+logData = []
+
+for subdir, dirs, files in os.walk(rootdir):
+	for file in files:
+		fullPath = os.path.join(subdir, file)
+		for ext in extToDelete:
+			if fullPath.endswith(ext):
+				logData.append('Removing -> ' + fullPath + '\n')
+				os.remove(fullPath)
+	for directories in dirs:
+		fullPath = os.path.join(subdir, directories)
+		if not os.listdir(fullPath):
+			os.rmdir(fullPath)
+with open(rootdir+'/log', 'w') as logfile:
+			for line in logData:
+				logfile.write(line)
