664a5d0a141d3bd3f749e0eef63eb67f481a7fac
Adrián Arbós <adrianarbosnavas@users.noreply.github.com>
2017-12-01 12:28:57 +0100

    Update removeByExtension.py

--- removeByExtension.py
+++ removeByExtension.py
@@ -16,10 +16,13 @@ for subdir, dirs, files in os.walk(rootdir):
 			if fullPath.endswith(ext):
 				logData.append('Removing -> ' + fullPath + '\n')
 				os.remove(fullPath)
+				
+for subdir, dirs, files in os.walk(rootdir):
 	for directories in dirs:
 		fullPath = os.path.join(subdir, directories)
 		if not os.listdir(fullPath):
 			os.rmdir(fullPath)
+			
 with open(rootdir+'/log', 'w') as logfile:
 			for line in logData:
 				logfile.write(line)
